c3e8f43ba608b83fa5705eee9603b43211bd55f3
alan910127 <alan469357@gmail.com>
2022-11-13 15:34:23 +0800

    :art: Run code formatter

--- hw2/src/mainwindow.h
+++ hw2/src/mainwindow.h
@@ -1,54 +1,57 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-#include <QtDebug>
-#include <QMainWindow>
-#include <vector>
-#include <QMenu>
+
+#include <QComboBox>
+#include <QHeaderView>
 #include <QLabel>
 #include <QLayout>
 #include <QLineEdit>
+#include <QMainWindow>
+#include <QMenu>
 #include <QPushButton>
-#include <QTreeWidget>
 #include <QStringList>
-#include <QComboBox>
 #include <QTabWidget>
 #include <QTableWidget>
-#include <QHeaderView>
+#include <QTreeWidget>
+#include <QtDebug>
+#include <vector>
+
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-    QTreeWidget *local_site;
-    QComboBox *local_memo;
-    QStringList *local_memo_dir;
+class MainWindow : public QMainWindow {
+  Q_OBJECT
+  QTreeWidget* local_site;
+  QComboBox* local_memo;
+  QStringList* local_memo_dir;
 
-    QTreeWidget *remote_site;
-    QComboBox *remote_memo;
-    QStringList *remote_memo_dir;
+  QTreeWidget* remote_site;
+  QComboBox* remote_memo;
+  QStringList* remote_memo_dir;
 
-    QTreeWidget *local_file_view;
-    QTreeWidget *remote_file_view;
+  QTreeWidget* local_file_view;
+  QTreeWidget* remote_file_view;
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-    void mainmenu();
-    QHBoxLayout* input_field();
-    void local_view();
-    void remote_view();
-    QHBoxLayout* memo_box();
-    QWidget* status();
+ public:
+  MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
+  void mainmenu();
+  QHBoxLayout* input_field();
+  void local_view();
+  void remote_view();
+  QHBoxLayout* memo_box();
+  QWidget* status();
 
-public slots:
-    void click_item_inDir(QTreeWidgetItem *);
-    void dir_combo_changed(const QString&);
-    void remote_click_item_inDir(QTreeWidgetItem *);
-    void remote_dir_combo_changed(const QString&);
+ public slots:
+  void click_item_inDir(QTreeWidgetItem*);
+  void dir_combo_changed(const QString&);
+  void remote_click_item_inDir(QTreeWidgetItem*);
+  void remote_dir_combo_changed(const QString&);
 
-private:
-    Ui::MainWindow *ui;
+ private:
+  Ui::MainWindow* ui;
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
